7811c3782a2e53e5e9528375e6321f0b6668c120
Swadesh soni <78621166+captain-ss@users.noreply.github.com>
2025-09-11 20:15:30 +0530

    Update server port configuration to 3002

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Security middleware
 app.use(helmet());
